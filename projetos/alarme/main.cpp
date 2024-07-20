610b01938da9e7054ae5e57244c83b7e101bc1be
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-07-19 21:17:44 -0300

    Update main.cpp
    Alteração da lógica escrita com estrutura de repetição While para For.

--- projetos/alarme/main.cpp
+++ projetos/alarme/main.cpp
@@ -1,22 +1,17 @@
-// Declarando a variável "i" com o valor 100.
-int i = 100;
-
 void setup()
 {
   // Configurando o pino 10 como saída para o funcionamento do led.
   pinMode(10, OUTPUT);
   Serial.begin(9600); // Iniciando do monitor serial.
   
-  // Estrutura while para contar "i" até 1200.
-  while(i < 1200){
-    i = i + 1; // Acrescentanto 1 ao valor de "i".
+  // Estrutura for para contar "i" até 1200.
+  for(int i=100; i<=1200; i++) {
     Serial.println(i); // Print do valor de "i" no monitor serial.
-    // Ativando o alarme no pino 7 com a frenquência igual ao valor de "i".
-    tone(7, i); 
+    tone(7, i); // Ativando o alarme no pino 7 com a frenquência igual ao valor de "i".
   }
 }
 
 void loop()
 {
-  digitalWrite(10, 1); // Ativando o pino 10;
+  digitalWrite(10, 1); // Ativando o pino 10.
 }
